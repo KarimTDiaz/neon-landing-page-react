f7db27293a3b82419b758f5a2aca50bf4abd1fb0
Karim Tilouni Diaz <117732405+KarimTDiaz@users.noreply.github.com>
2023-03-24 22:00:44 +0100

    Neon landing coded with React

---
+++ src/constants/consts.js
@@ -0,0 +1,9 @@
+export const COLORS = {
+    bgColor:'rgb(17 24 39)',
+    titleColor:'rgb(243 244 246)',
+    textColor:'rgb(156 163 175)',
+    primaryColor:'#2563eb',
+    primaryColorSoft:'#60a5fa',
+    secondaryColor:'#1f2937',
+    secondaryColorSoft:'#374151'
+}
